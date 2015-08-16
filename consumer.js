c649973cd69bd03b1294aad98c84e0f81e8e19dd
Shahin <me@5hah.in>
2015-08-16 18:24:03 +0430

    Add analytics.

--- consumer.js
+++ consumer.js
@@ -1,3 +1,4 @@
+// Initialization
 var kue, config, request, queue, magfa;
 
 kue = require('kue');
@@ -6,6 +7,10 @@ config = require('./configs/config');
 magfa = config.magfa;
 
 queue = kue.createQueue();
+
+// Setup analytics
+var Analytics = require('analytics-node');
+var analytics = new Analytics('YOUR_WRITE_KEY');
 
 queue.process("send-sms", function(job, done){
     send_sms(job.data, done);
@@ -33,8 +38,20 @@ var send_sms = function(data, done){
     req = request.post(endpoint, function(error, response, body){
         if(!error && response.statusCode === 200){
             console.log("Add messsage " + body + " to check status queue");
-            var msg_data = {mid: body};
+            var msg_data = {mid: body, user_id: data.user_id};
             queue.create("check-status", msg_data).save();
+
+            // If there is a user_id, track status
+            if(data.user_id){
+                analytics.track({
+                    userId: data.user_id,
+                    event: 'SMS Sent',
+                    properties: {
+                        message_id: data.mid,
+                        status: "Message in queue" 
+                    }
+                });
+            }
         } else {
             console.log("I failed");
         }
@@ -56,6 +73,27 @@ var check_status = function(data, done){
 
     req = request.post(endpoint, function(error, response, body){
         if(!error && response.statusCode === 200){
+            var status;
+           
+            if(body == 1){
+                status = "Successful";
+            } else if(body in [0, 2, 8, 16]){
+                status = "Processing";
+            } else {
+                status = "Failed";
+            }
+
+            // If there is a user_id, track status
+            if(data.user_id){
+                analytics.track({
+                    userId: data.user_id,
+                    event: 'SMS Sent',
+                    properties: {
+                        message_id: data.mid,
+                        status: status 
+                    }
+                });
+            }
             console.log("Change message status to " + body);
         } else {
             console.log("I failed");
