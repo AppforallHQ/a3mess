6505f64ab9220db50c258abd0233986683d71dbf
Shahin <me@5hah.in>
2015-08-22 11:56:06 +0430

    Add necessary tools to handle SOAP

---
+++ render.js
@@ -0,0 +1,55 @@
+var jade = require("jade");
+
+var actions = function(){
+    this.data = {
+        domainName: "magfa",
+        senderNumbers: [""]
+    };
+
+    this.files = {
+        enqueue: "templates/enqueue.jade",
+        getRealMessageStatuses: "templates/getRealMessageStatuses.jade",
+        getCredit: "templates/getCredit.jade"
+    };
+
+    this.options = {
+        // pretty: true
+    };
+};
+
+actions.prototype.enqueue = function(messageBodies, recipientNumbers){
+    var engine;
+
+    if(!messageBodies || typeof messageBodies !== "object" ||
+       !recipientNumbers || typeof recipientNumbers !== "object"){
+        return null;
+    }
+
+    this.data.messageBodies = messageBodies;
+    this.data.recipientNumbers = recipientNumbers;
+
+    engine = jade.compileFile(this.files.enqueue, this.options);
+    return engine(this.data);
+};
+
+actions.prototype.getRealMessageStatuses = function(messageIds){
+    var engine;
+
+    if(!messageIds || typeof messageIds !== "object"){
+        return null;
+    }
+
+    this.data.messageIds = messageIds;
+
+    engine = jade.compileFile(this.files.getRealMessageStatuses, this.options);
+    return engine(this.data);
+};
+
+actions.prototype.getCredit = function(){
+    var engine;
+
+    engine = jade.compileFile(this.files.getCredit, this.options);
+    return engine(this.data);
+};
+
+module.exports = actions;
